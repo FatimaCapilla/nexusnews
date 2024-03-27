0488ec77a49a00583bfc668542ba4ce8c56e1c4e
FatimaCapilla <fatima.capilla.sancho@gmail.com>
2024-03-27 14:58:33 +0100

    nav and footer

--- client/src/layout/LayoutPublic.jsx
+++ client/src/layout/LayoutPublic.jsx
@@ -1,9 +1,13 @@
+import Footer from '../components/footer/Footer.jsx'
+import Nav from '../components/nav/Nav.jsx';
 import { Outlet } from 'react-router-dom';
 
 const LayoutPublic = () => {
   return (
     <>
+    < Nav />
     < Outlet />
+    < Footer />
     </>
   )
 }
