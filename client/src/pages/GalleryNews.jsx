9dc8f23700bdc364681f2370fe744ab97f18e6ec
elenalope <elopmorales97@gmail.com>
2024-04-03 11:01:34 +0200

    gallery news done well

--- client/src/pages/GalleryNews.jsx
+++ client/src/pages/GalleryNews.jsx
@@ -16,17 +16,18 @@ const GalleryNews = () => {
 
     return (
       <>
-      <div className="header">
-        <h3>Tecnología - Últimas noticias</h3>
-        <button>Añadir noticia</button>
+      <div className="gallery-header">
+        <h3 className="gallery-h3">Tecnología - Últimas noticias</h3>
+        <button className="gallery-button">Añadir noticia</button>
       </div>
 
       <div className="gallery">
         {news.map((item, index) => (
-          <div key={index} className="news-item">
+          <div key={index} className="gallery-news-item">
             <img src={item.image} />
-            <div className="news-details">
+            <div className="gallery-news-details">
             <h4>{item.title}</h4>
+            <img className="arrow" src={} />
             <p>{item.date}</p>
           </div>
           </div>
