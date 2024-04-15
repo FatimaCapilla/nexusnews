8079aa10ad109e406ee14b7ae104f2b1cea363c7
mariandrean <m.andreaan@gmail.com>
2024-04-15 12:07:14 +0200

    fix(News.jsx):add-role-verification-to-delete-button

--- client/src/pages/News.jsx
+++ client/src/pages/News.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from 'react';
-import { getOneNews, deleteNews } from '../services/newsServices'; 
+import { getOneNews, deleteNews } from '../services/newsServices';
 import { useParams } from "react-router";
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import "../pages/News.css"
 
 const News = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [data, setData] = useState(null);
+  const [userRole, setUserRole] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getOneNews(id); 
-      setData(response); 
+      const response = await getOneNews(id);
+      setData(response);
     }
+    const userRole = localStorage.getItem('role');
+    setUserRole(userRole);
     fetchData()
   }, [id]);
 
   const handleDelete = async () => {
     try {
-      await deleteNews(id); 
+      await deleteNews(id);
       navigate("/gallery");
     } catch (error) {
       console.error(error);
@@ -28,11 +31,13 @@ const News = () => {
 
   return (
     <div className='news-model'>
-      <button className="delete-button" onClick={handleDelete}>Eliminar</button>
+      {userRole === 'admin' && (
+        <button className="delete-button" onClick={handleDelete}>Eliminar</button>
+      )}
       {data ? (
         <article className='news' key={data.id}>
           <h1 className='title-news'>{data.title}</h1><br />
-          <div className='image-news'><img src={data.image} alt='img-news' className='image'/></div><br />
+          <div className='image-news'><img src={data.image} alt='img-news' className='image' /></div><br />
           <div className='date'><span className='strong'></span>{data.date}</div>
           <h3 className='body-news'><span className='body'>{data.body}</span></h3>
         </article>
