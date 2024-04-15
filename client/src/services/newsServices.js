d35829c5adc4cfda5e3f773cc6e51dc3f66f26f7
BeaCp123 <beatrizcustodio262@gmail.com>
2024-04-15 09:43:13 +0200

    feature(client):Get-Method-Ok

--- client/src/services/newsServices.js
+++ client/src/services/newsServices.js
@@ -1,16 +1,18 @@
-const url = 'http://www.localhost:3000/api/news';
+import axios from "axios";
+const URLAPI_NEWS = 'http://www.localhost:3000/api/news';
 
 // Función para obtener las noticias
 export const getNews = async () => {
+  const token = localStorage.getItem('token')
+  console.log(token)
+  const headers = {'Authorization': `Bearer ${token}`}
   try {
-    const response = await fetch(url);
-    
-    if (!response.ok) {
-      throw new Error('Error al obtener las noticias');
-    }
-    
-    const data = await response.json();
-    return data;
+    const response = await axios.get(URLAPI_NEWS,{headers} );
+    console.log(response)
+    // if (!response.ok) {
+    //   throw new Error('Error al obtener las noticias');
+    // }
+    return response.data
   } catch (error) {
     console.error('Error fetching news:', error);
     throw error;
