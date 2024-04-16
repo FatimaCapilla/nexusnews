1b9079153cf28ae51b53d3b537d454116c9bfcb1
elenalope <elopmorales97@gmail.com>
2024-04-16 12:09:12 +0200

    newsServices corrected

--- client/src/services/newsServices.js
+++ client/src/services/newsServices.js
@@ -2,12 +2,28 @@ import axios from "axios";
 
 const URLAPI_NEWS = 'http://localhost:3000/api/news/';
 
+export const getNews = async () => {
+  const token = localStorage.getItem('token');
+  const headers = { 'Authorization': `Bearer ${token}` }
+  try {
+    const response = await axios.get(URLAPI_NEWS, { headers });
+    console.log(response)
+    // if (!response.ok) {
+    //   throw new Error('Error al obtener las noticias');
+    // }
+    return response.data
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    throw error;
+  }
+};
+
 export const getOneNews = async (id) => {
   const token = localStorage.getItem('token');
-  const headers = { 'Authorization': `Bearer ${token}` };
+  const headers = { 'Authorization': `Bearer ${token}` }
   try {
-    const response = await axios.get(`${URLAPI_NEWS}${id}`, { headers });
-    return response.data;
+    const response = await axios.get(`${URLAPI_NEWS + id}`, { headers });
+    return response.data
   } catch (error) {
     console.error('Error fetching news:', error);
     throw error;
@@ -18,44 +34,13 @@ export const deleteNews = async (id) => {
   try {
     const token = localStorage.getItem('token');
     const headers = { 'Authorization': `Bearer ${token}` };
-    const confirmDelete = window.confirm("¿Estás seguro que deseas borrar la noticia?");
-    if (confirmDelete) {
-      const response = await axios.delete(`${URLAPI_NEWS}${id}`, { headers });
-      if (response.status === 200) {
-        alert('Eliminado correctamente');
-      }
-    }
+    const response = await axios.delete(`${URLAPI_NEWS}${id}`, { headers });
+    return response.data;
   } catch (error) {
     console.error('Error deleting news:', error);
     throw error;
   }
-};
-
-// Función para obtener todas las noticias
-export const getNews = async () => {
-  const token = localStorage.getItem('token');
-  const headers = { 'Authorization': `Bearer ${token}` };
-  try {
-    const response = await axios.get(URLAPI_NEWS, { headers });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching news:', error);
-    throw error;
-  }
-};
-
-// Función para obtener una noticia por su ID
-export const getById = async (id) => {
-  const token = localStorage.getItem('token');
-  const headers = { 'Authorization': `Bearer ${token}` };
-  try {
-    const response = await axios.get(`${URLAPI_NEWS}${id}`, { headers });
-    return response.data;
-  } catch (error) {
-    console.error(`Error fetching news with ID ${id}:`, error);
-    throw error;
-  }
-};
+}
 
 export const updateNews = async (id, updatedData) => {
   const token = localStorage.getItem('token');
